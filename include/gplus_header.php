700da3ed37d4be1b7769ab51ca40fec2f9794721
SolarLiner <solarliner@gmail.com>
2014-06-17 23:24:29 +0200

    Search feature successul (I think)

--- include/gplus_header.php
+++ include/gplus_header.php
@@ -6,15 +6,15 @@
 	<div class="col-lg-12">
 		<div class="navbar-header">
 			<a class="navbar-brand" href="/">kOS Repo</a>
+			<a class="navbar-brand" href="#search" ><i class="glyphicon glyphicon-search"></i></a>
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse1">
-				<i class="glyphicon glyphicon-search"></i>
+				<i class="glyphicon glyphicon-th-large"></i>
 			</button>
 		</div>
 		<div id="navbar-collapse1" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="register.php">Register</a></li>
 				<li><a href="login.php">Log in</a></li>
-				<li><a href="#search">Search</a></li>
 			</ul>
 		</div>
 	</div>
